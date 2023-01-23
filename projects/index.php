dd4c11f6890f26b3ea803e91ece7d77d678a5c6d
jessarock <jesrock87@gmail.com>
2023-01-22 20:58:20 -0700

    Business card site updates

--- projects/index.php
+++ projects/index.php
@@ -24,14 +24,14 @@
   <section>
     <inner-column>
     	<ul>
-    		<li><a href="https://peprojects.dev/alpha-6/jess/projects/disney-freaks/">mock client</a></li>
-    		<li><a href="https://peprojects.dev/alpha-6/jess/projects/four-page-site/">four-page-site</a></li>
-    		<li><a href="https://peprojects.dev/alpha-6/jess/projects/portal/">web portal</a></li>
-    		<li><a href="https://peprojects.dev/alpha-6/jess/projects/super-layout-challenge/">responsive layout challenge part 1 </a></li>
-    		<li><a href="https://peprojects.dev/alpha-6/jess/projects/second-layout-challenge/index.html">responsive layout challenge part 2</a></li>
-    		<li><a href="https://peprojects.dev/alpha-6/jess/monsters/">monster adoption agency</a></li>
-    		<li><a href="https://peprojects.dev/alpha-6/jess/projects/e4p/">exercises for programmers</a></li>
-    		<li><a href="https://peprojects.dev/alpha-6/jess/projects/cocktails/?page=home">learning dynamic templating</a></li>
+    		<li><a href="https://peprojects.dev/alpha-6/jess/projects/disney-freaks/" target="_blank">mock client</a></li>
+    		<li><a href="https://peprojects.dev/alpha-6/jess/projects/four-page-site/" target="_blank">four-page-site</a></li>
+    		<li><a href="https://peprojects.dev/alpha-6/jess/projects/portal/" target="_blank">web portal</a></li>
+    		<li><a href="https://peprojects.dev/alpha-6/jess/projects/super-layout-challenge/" target="_blank">responsive layout challenge part 1 </a></li>
+    		<li><a href="https://peprojects.dev/alpha-6/jess/projects/second-layout-challenge/index.html" target="_blank">responsive layout challenge part 2</a></li>
+    		<li><a href="https://peprojects.dev/alpha-6/jess/monsters/" target="_blank">monster adoption agency</a></li>
+    		<li><a href="https://peprojects.dev/alpha-6/jess/projects/e4p/" target="_blank">exercises for programmers</a></li>
+    		<li><a href="https://peprojects.dev/alpha-6/jess/projects/cocktails/?page=home" target="_blank">learning dynamic templating</a></li>
     		</ul>
    
      </inner-column>
