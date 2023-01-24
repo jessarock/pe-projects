962db0c376d208e61dbf56364e911f994241ce76
jessarock <jesrock87@gmail.com>
2023-01-23 22:44:49 -0700

    Theme challenge

---
+++ projects/theme-challenge/head.php
@@ -0,0 +1,10 @@
+	<head>
+			<meta charset="utf-8">
+			<meta name="viewport" content="width=device-width, initial-scale=1">
+
+			<title>Theme Challenge</title>
+			<meta name="description" content="super ultra responsive layout and theming challenge">
+			<meta property="og:image" content="images/meta-default.png">
+			
+			<link rel="stylesheet" href="css/site.css">
+		</head>
