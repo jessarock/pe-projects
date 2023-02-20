d6369c7157d846af59981f5093c6aa14d7156e54
jessarock <jesrock87@gmail.com>
2023-02-19 19:17:23 -0700

    Added json decode function

--- version-two/templates/pages/welcome.php
+++ version-two/templates/pages/welcome.php
@@ -1,6 +1,18 @@
 
+
+
+<?php
+
+	$pageData = getData("data/pages/home.json");
+
+	formatCode($pageData);
+
+	?>
+
+
+
 <section>
 	<inner-column>
-		<h1 class="loud-voice">welcome</h1>
+		<h1 class="loud-voice"><?=$pageData["heading"]?></h1>
 	</inner-column>
 </section>
